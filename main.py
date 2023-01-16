c8e91b5da4c42e3d2b5076eac3f3db59cebf4644
sabaghip <sabaghi.p@gmail.com>
2023-01-16 15:17:00 +0330

    fourier transform added

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ import matplotlib.pyplot as plt
 PATH = "F:/parsa/daneshgah/signal/Project"
 
 sr, sig_audio = wavfile.read(PATH + "/Voices/1.wav")
+time = sig_audio.shape[0]/sr
+sample = sig_audio.shape[0]
+t = np.arange(0, time, 1/sr)
 
-sig_out = sig_audio
+sig_fr = np.fft.fft(sig_audio)
+
+sig_out = np.fft.ifft(sig_fr)
+
+sig_out = sig_out.astype(sig_audio.dtype)
 
 wavfile.write(PATH + "/Voices/output.wav", sr, sig_out.astype(sig_audio.dtype))
